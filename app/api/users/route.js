80746468560e02f32fde73f89524a92a1cd42b43
Moshe Klienblut <moshek@college.labs>
2026-01-12 13:05:08 +0200

    phase 2

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,10 +1,15 @@
 export const runtime = "nodejs";
 
 import { createUser} from "@/lib/users";
+import { hash } from "bcrypt"
 import { NextResponse } from "next/server";
 
-export async function POST() {
-  const user = await createUser();
+
+export async function POST(req) {
+  const {email, password, name, phone} = await req.json()
+  const hashedPassword = await hash(password,10)
+  const userInfo = {email:email, password:hashedPassword, name:name, phone:phone}
+  const user = await createUser(userInfo);
   return NextResponse.json({ message: "success", id: user.id });
 }
 
